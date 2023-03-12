fe232a74b2e90bfd1162d25a7949daf662add989
h-gj <690631890@qq.com>
2023-03-12 13:44:21 +0800

    refactor to be compatible with any text replace

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { useState, useEffect, useRef } from 'react';
 
 function App() {
   const [curl, setCurl] = useState('')
+  const [textForReplace, setTextForReplace] = useState('')
+  const [replaceChoice, setReplaceChoice] = useState(1)
+  const [textReplaceTo, setTextReplaceTo] = useState('http://127.0.0.1:8000')
   const [newCurl, setNewCurl] = useState('')
   const [port, setPort] = useState(8000)
   const [host, setHost] = useState('http://127.0.0.1')
   const textAreaRef = useRef(null);
   const curlRef = useRef(null);
 
-  useEffect((e) => {
-    geneReplacedCurl(curl)
-   },[host, port])
+  // useEffect((e) => {
+  //   replaceAndCopy()
+  //  },[textForReplace, textReplaceTo, replaceChoice])
 
   const cuzSetPort = (e) => {
     const newPort = e.target.value
@@ -31,16 +34,43 @@ function App() {
     const matchedUrl = oldCurl.match('https?://.*')[0]
     const url = new URL(matchedUrl)
 
-    const newCurl = oldCurl.replace(url.origin, `${host}:${port}`)
-    setNewCurl(newCurl)
-    copyToClipboard(newCurl)
+    setTextForReplace(url.origin)
+    // setTextReplaceTo(`${host}:${port}`)
+    // const newCurl = oldCurl.replace(textForReplace, textReplaceTo)
+    // const newCurl = oldCurl.replace(url.origin, `${host}:${port}`)
+    // setNewCurl(newCurl)
+    // copyToClipboard(newCurl)
+  }
+
+  const handleTextForReplaceChange = (e) => {
+    setTextForReplace(e.target.value)
+    // geneReplacedCurl()
+  }
+
+  const handleTextReplaceToChange = (e) => {
+    console.log(232323);
+    setTextReplaceTo(e.target.value)
+    // geneReplacedCurl()
+  }
+
+  const replaceAndCopy = (e) => {
+    console.log(22222, curl)
+    let newOne
+    if (replaceChoice === 1) {
+      newOne = curl.replace(textForReplace, textReplaceTo)
+    } else {
+      newOne = curl.replaceAll(textForReplace, textReplaceTo)
+    }
+    // const newCurl = oldCurl.replace(url.origin, `${host}:${port}`)
+    setNewCurl(newOne)
+    copyToClipboard(newOne)
   }
 
   const copyToClipboard = (text) => {
     const textArea = document.createElement("textarea");
     textArea.value = text;
     document.body.appendChild(textArea);
-    textArea.focus();
+    // textArea.focus();
     textArea.select();
     try {
       document.execCommand('copy');
@@ -53,7 +83,7 @@ function App() {
   const onClear = (e) => {
     console.log(2323232, e, curlRef);
     setCurl('')
-    curlRef.current.focus()
+    // curlRef.current.focus()
   }
 
 
@@ -69,7 +99,7 @@ function App() {
         >
         <textarea 
         ref={curlRef}
-        placeholder="请粘贴Curl" 
+        placeholder="Paste raw text here." 
         rows="40"
         cols="55"
         autoFocus
@@ -84,14 +114,57 @@ function App() {
           flexDirection: "column"
         }}
         >
+      
+        {/* <br /> */}
+
+        Text for Replace:
+        <input 
+        value={textForReplace}
+        onChange={(e) => setTextForReplace(e.target.value)}
+        style={{padding: 10}}
+        >
+        </input>
+
+        Replace to:
+        <input 
+        value={textReplaceTo}
+        onChange={(e) => setTextReplaceTo(e.target.value)}
+        style={{padding: 10}}
+        >
+        </input>
+
+        <div>
+        <label for="replaceChoice">replace one</label>
+        <input type="radio" id="replaceChoice" checked={replaceChoice === 1} name="replaceChoice" value="replace one" onChange={() => setReplaceChoice(1)} />
+
+        <label for="replaceChoice">replace all</label>
+        <input type="radio" id="replaceChoice" checked={replaceChoice === 2} name="replaceChoice" value="replace all" onChange={() => setReplaceChoice(2)} />
+        </div>
+
+        <br />
+      
+
+        {/* <input type="radio" value="replace all " /> */}
+
         <button 
         onClick={onClear}
         style={{
           padding: 20
         }}
-        >Clear</button>
-        <br />
-        Map to host:
+        >Clear Left</button>
+        
+        <button
+          onClick={replaceAndCopy}
+          style={{
+          padding: 20
+        }}
+         >Gene & Copy
+        </button> 
+        {/* Result will be auto copied! */}
+
+
+
+        {/* Map to host:
 
         <input 
         value={host}
@@ -111,7 +184,7 @@ function App() {
          </input>
          <button
           onClick={() => setPort(prev => prev + 1)}
-         >+</button>
+         >+</button> */}
 
 
         </div>
