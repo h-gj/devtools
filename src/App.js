b4e141d6f14f539f4d0657825d6fa498fb30e2ff
h-gj <690631890@qq.com>
2023-03-21 23:40:22 +0800

    style: clean code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useRef } from 'react';
 
 function App() {
   const [curl, setCurl] = useState('')
@@ -6,62 +6,28 @@ function App() {
   const [replaceChoice, setReplaceChoice] = useState(1)
   const [textReplaceTo, setTextReplaceTo] = useState('http://127.0.0.1:8000')
   const [newCurl, setNewCurl] = useState('')
-  const [port, setPort] = useState(8000)
-  const [host, setHost] = useState('http://127.0.0.1')
   const textAreaRef = useRef(null);
   const curlRef = useRef(null);
 
-  // useEffect((e) => {
-  //   replaceAndCopy()
-  //  },[textForReplace, textReplaceTo, replaceChoice])
-
-  const cuzSetPort = (e) => {
-    const newPort = e.target.value
-    setPort(newPort)
-    // 为什么这样不行，数据会慢一步，useEffect可以。
-    // console.log(111123232, newPort, port);
-    // geneReplacedCurl(curl)
-  }
-
   const geneReplacedCurl = (oldCurl) => {
     setCurl(oldCurl)
-
-    // const oldCurl = e.target.value
     if (!oldCurl) {
       return
     }
 
     const matchedUrl = oldCurl.match('https?://.*')[0]
     const url = new URL(matchedUrl)
-
     setTextForReplace(url.origin)
-    // setTextReplaceTo(`${host}:${port}`)
-    // const newCurl = oldCurl.replace(textForReplace, textReplaceTo)
-    // const newCurl = oldCurl.replace(url.origin, `${host}:${port}`)
-    // setNewCurl(newCurl)
-    // copyToClipboard(newCurl)
   }
 
-  const handleTextForReplaceChange = (e) => {
-    setTextForReplace(e.target.value)
-    // geneReplacedCurl()
-  }
-
-  const handleTextReplaceToChange = (e) => {
-    console.log(232323);
-    setTextReplaceTo(e.target.value)
-    // geneReplacedCurl()
-  }
 
   const replaceAndCopy = (e) => {
-    console.log(22222, curl)
     let newOne
     if (replaceChoice === 1) {
       newOne = curl.replace(textForReplace, textReplaceTo)
     } else {
       newOne = curl.replaceAll(textForReplace, textReplaceTo)
     }
-    // const newCurl = oldCurl.replace(url.origin, `${host}:${port}`)
     setNewCurl(newOne)
     copyToClipboard(newOne)
   }
@@ -83,7 +49,6 @@ function App() {
   const onClear = (e) => {
     console.log(2323232, e, curlRef);
     setCurl('')
-    // curlRef.current.focus()
   }
 
 
@@ -144,8 +109,6 @@ function App() {
         <br />
       
 
-        {/* <input type="radio" value="replace all " /> */}
-
         <button 
         onClick={onClear}
         style={{
@@ -160,40 +123,8 @@ function App() {
         }}
          >Gene & Copy
         </button> 
-        {/* Result will be auto copied! */}
-
-
-
-        {/* Map to host:
-
-        <input 
-        value={host}
-        onChange={(e) => setHost(e.target.value)}
-        >
-        </input>
-         With port：
-         
-         <button
-         onClick={() => setPort(prev => prev - 1)}
-         >-</button>
-         <input
-         placeholder="请输入本地端口" 
-         value={port}
-         onChange={cuzSetPort}
-         >
-         </input>
-         <button
-          onClick={() => setPort(prev => prev + 1)}
-         >+</button> */}
-
 
         </div>
-
-        {/* <span>8000</span>
-        <span>8001</span>
-        <span>8002</span>
-        <span>8003</span>
-        <span>8000</span> */}
 
         <textarea 
         value={newCurl}
@@ -202,7 +133,6 @@ function App() {
         ></textarea>
         </div>
 
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
 
       <form>
